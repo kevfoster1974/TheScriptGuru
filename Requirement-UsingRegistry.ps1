f8aec50a90d558fa0d00e3b757a8b400b67dd55e
kevfoster1974 <122815761+kevfoster1974@users.noreply.github.com>
2023-01-23 21:14:00 +0000

    Add files via upload

---
+++ Requirement-UsingRegistry.ps1
@@ -0,0 +1,12 @@
+﻿$AppName = "*Firefox*"
+$Path = "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall"
+$subkeys = Get-ChildItem -Path $Path 
+foreach($subkey in $subkeys)
+{
+    $displayname = (Get-ItemProperty -Path $subkey.PsPath).DisplayName
+    if($displayname -like $AppName)
+    {
+        Write-Host "Required"
+        break
+    }
+}
