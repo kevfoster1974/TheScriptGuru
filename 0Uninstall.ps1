6821e608d527238eaf2efc1acd76c8a1934d926e
kevfoster1974 <122815761+kevfoster1974@users.noreply.github.com>
2023-01-25 07:52:43 +0000

    Add files via upload

---
+++ 0Uninstall.ps1
@@ -0,0 +1,45 @@
+﻿# Remediation script
+# Check if WireSharkis installed if so ... then this script will remove it
+ 
+# Author: Kevin Foster
+# Date: 24th January 2023
+# Version: 1.00
+ 
+#-----------------------------[Parameters]----------------------------------
+$Arguments = "/S"
+$AppName = "*Wireshark*"
+$Path = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\*"
+$wowPath = "HKLM:\SOFTWARE\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\*"
+$subkeys = Get-ChildItem -Path $Path 
+$flag = $false
+
+#-----------------------------[Script]----------------------------------
+
+# Check the main registry key
+foreach($subkey in $subkeys)
+{
+    $displayname = (Get-ItemProperty -Path $subkey.PsPath).DisplayName
+    if($displayname -like $AppName)
+    {
+        $uninstallstring = (Get-ItemProperty -Path $subkey.PsPath).UninstallString
+        Start-Process $uninstallstring -ArgumentList $Arguments -Wait -NoNewWindow
+        $flag = $true
+        break
+    }
+}
+
+# If not found check the Wow6432Node registry key
+if (!$flag) {
+    $wowSubkeys = Get-ChildItem -Path $wowPath 
+    foreach($subkey in $wowSubkeys)
+    {
+        $displayname = (Get-ItemProperty -Path $subkey.PsPath).DisplayName
+        if($displayname -like $AppName)
+        {
+            $uninstallstring = (Get-ItemProperty -Path $subkey.PsPath).UninstallString
+            Start-Process $uninstallstring -ArgumentList $Arguments -Wait -NoNewWindow
+            $flag = $true
+            break
+        }
+    }
+}
